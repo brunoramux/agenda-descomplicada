16d329d23753466752b088045279883ccbdc191a
brunoramux <bruno.lemos@live.com>
2023-10-15 12:41:33 -0300

    Deploy to vercel

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -141,14 +141,12 @@ export default function TimeIntervals() {
                 </IntervalDay>
                 <IntervalInputs>
                   <TextInput
-                    size="sm"
                     type="time"
                     step={60}
                     disabled={intervals[index].enabled === false}
                     {...register(`intervals.${index}.startTime`)}
                   />
                   <TextInput
-                    size="sm"
                     type="time"
                     step={60}
                     disabled={intervals[index].enabled === false} // habilita e desabilita conforme estado do checkbox
